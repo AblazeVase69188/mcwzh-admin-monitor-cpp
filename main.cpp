98a7ae5e4346a70d73508037fa7daa0668d108e6
AblazeVase69188 <73418189+AblazeVase69188@users.noreply.github.com>
2025-07-22 18:44:14 +0800

    程序初步具备功能，可以格式化输出新内容并包含链接

--- main.cpp
+++ main.cpp
@@ -3,7 +3,111 @@
 #include <nlohmann/json.hpp>
 #include <cpr/cpr.h>
 constexpr auto WIKI_API_URL = "https://zh.minecraft.wiki/api.php";
+constexpr auto WIKI_DIFF_URL = "https://zh.minecraft.wiki/?diff=";
+constexpr auto WIKI_LOG_URL = "https://zh.minecraft.wiki/w/Special:%E6%97%A5%E5%BF%97/";
+constexpr auto WIKI_AFL_URL = "https://zh.minecraft.wiki/w/Special:%E6%BB%A5%E7%94%A8%E6%97%A5%E5%BF%97/";
 using json = nlohmann::json; // 简化命名空间
+
+// 全局变量
+json LOG_TYPE_MAP;
+json LOG_ACTION_MAP;
+json AF_ACTION_MAP;
+json AF_RESULT_MAP;
+
+// 定义UTF-8字符串
+std::string LOGIN_TOKEN_FAIL(reinterpret_cast<const char*>(u8"登录令牌获取失败"));
+std::string LOGIN_TOKEN_SUCCESS(reinterpret_cast<const char*>(u8"登录令牌获取成功"));
+std::string LOGIN_FAIL(reinterpret_cast<const char*>(u8"登录失败"));
+std::string LOGIN_SUCCESS(reinterpret_cast<const char*>(u8"登录成功"));
+std::string INITIAL_FAIL(reinterpret_cast<const char*>(u8"获取初始数据失败"));
+std::string STARTUP_SUCCESS(reinterpret_cast<const char*>(u8"启动成功"));
+std::string REQUEST_FAIL(reinterpret_cast<const char*>(u8"获取数据失败"));
+std::string ZH_COMMA(reinterpret_cast<const char*>(u8"，"));
+
+// 函数声明
+// 调整时间戳为UTC+8
+static std::string adjust_timestamp(const std::string& timestamp) {
+	std::string hour_str = timestamp.substr(11, 2);
+	int hour = std::stoi(hour_str);
+    hour = (hour + 8) % 24;
+	std::string adjusted_str = (hour < 10 ? "0" : "") + std::to_string(hour) + timestamp.substr(13,6);
+    return adjusted_str;
+}
+// 调整摘要输出
+static void print_comment(const json& item) {
+    const auto& comment = item["comment"].get<std::string>();
+    if (!comment.empty()) {
+        std::cout << comment;
+    }
+    else {
+        std::cout << std::string(reinterpret_cast<const char*>(u8"（空）"));
+    }
+}
+// 输出最近更改url
+static void print_rc_url(const json& item) {
+    if (item["revid"].get<int>() == 0) {
+        std::cout << WIKI_LOG_URL << item["logtype"].get<std::string>();
+    }
+    else {
+        std::cout << WIKI_DIFF_URL << item["revid"].get<int>();
+    }
+}
+// 输出滥用日志url
+static void print_afl_url(const json& item) {
+    std::cout << WIKI_AFL_URL << item["id"].get<int>();
+}
+// 打印最近更改
+static void printrc(const json& item) {
+    if (item["type"].get<std::string>() == "log") {
+        std::cout << std::string(reinterpret_cast<const char*>(u8"（")) << LOG_TYPE_MAP[item["logtype"].get<std::string>()].get<std::string>() << std::string(reinterpret_cast<const char*>(u8"）"));
+        std::cout << adjust_timestamp(item["timestamp"].get<std::string>());
+        std::cout << ZH_COMMA;
+        std::cout << item["user"].get<std::string>() << std::string(reinterpret_cast<const char*>(u8"对")) << item["title"].get<std::string>() << std::string(reinterpret_cast<const char*>(u8"执行了")) << LOG_ACTION_MAP[item["logaction"].get<std::string>()].get<std::string>() << std::string(reinterpret_cast<const char*>(u8"操作"));
+        std::cout << ZH_COMMA;
+        std::cout << std::string(reinterpret_cast<const char*>(u8"摘要为"));
+        print_comment(item);
+        std::cout << std::endl;
+		print_rc_url(item);
+        std::cout << std::endl << std::endl;
+    }
+    else if (item["type"].get<std::string>() == "edit") {
+        std::cout << adjust_timestamp(item["timestamp"].get<std::string>());
+        std::cout << ZH_COMMA;
+        std::cout << item["user"].get<std::string>() << std::string(reinterpret_cast<const char*>(u8"在")) << item["title"].get<std::string>() << std::string(reinterpret_cast<const char*>(u8"做出编辑"));
+        std::cout << ZH_COMMA;
+        std::cout << std::string(reinterpret_cast<const char*>(u8"字节更改为")) << item["newlen"].get<int>() - item["oldlen"].get<int>();
+        std::cout << ZH_COMMA;
+        std::cout << std::string(reinterpret_cast<const char*>(u8"摘要为"));
+        print_comment(item);
+        std::cout << std::endl;
+        print_rc_url(item);
+        std::cout << std::endl << std::endl;
+    }
+    else {
+        std::cout << adjust_timestamp(item["timestamp"].get<std::string>());
+        std::cout << ZH_COMMA;
+        std::cout << item["user"].get<std::string>() << std::string(reinterpret_cast<const char*>(u8"创建")) << item["title"].get<std::string>();
+        std::cout << ZH_COMMA;
+        std::cout << std::string(reinterpret_cast<const char*>(u8"字节更改为")) << item["newlen"].get<int>() - item["oldlen"].get<int>();
+        std::cout << ZH_COMMA;
+        std::cout << std::string(reinterpret_cast<const char*>(u8"摘要为"));
+        print_comment(item);
+        std::cout << std::endl;
+        print_rc_url(item);
+        std::cout << std::endl << std::endl;
+    }
+}
+// 打印滥用日志
+static void printafl(const json& item) {
+    std::cout << adjust_timestamp(item["timestamp"].get<std::string>());
+    std::cout << ZH_COMMA;
+    std::cout << item["user"].get<std::string>() << std::string(reinterpret_cast<const char*>(u8"在")) << item["title"].get<std::string>() << std::string(reinterpret_cast<const char*>(u8"执行操作")) << AF_ACTION_MAP[item["action"].get<std::string>()].get<std::string>() << std::string(reinterpret_cast<const char*>(u8"时触发了过滤器")) << item["filter"].get<std::string>();
+    std::cout << ZH_COMMA;
+	std::cout << std::string(reinterpret_cast<const char*>(u8"采取的行动为")) << AF_RESULT_MAP[item["result"].get<std::string>()].get<std::string>();
+    std::cout << std::endl;
+	print_afl_url(item);
+	std::cout << std::endl << std::endl;
+}
 
 int main() {
     // 设置控制台为UTF-8编码
@@ -25,19 +129,10 @@ int main() {
         std::string password = config["password"];
 
         // 加载系统消息
-		json LOG_TYPE_MAP = config["log_type_map"];
-		json LOG_ACTION_MAP = config["log_action_map"];
-		json AF_ACTION_MAP = config["af_action_map"];
-		json AF_RESULT_MAP = config["af_result_map"];
-
-        // 加载提示信息
-        std::string LOGIN_TOKEN_FAIL(reinterpret_cast<const char*>(u8"登录令牌获取失败"));
-        std::string LOGIN_TOKEN_SUCCESS(reinterpret_cast<const char*>(u8"登录令牌获取成功"));
-        std::string LOGIN_FAIL(reinterpret_cast<const char*>(u8"登录失败"));
-        std::string LOGIN_SUCCESS(reinterpret_cast<const char*>(u8"登录成功"));
-        std::string INITIAL_FAIL(reinterpret_cast<const char*>(u8"获取初始数据失败"));
-        std::string STARTUP_SUCCESS(reinterpret_cast<const char*>(u8"启动成功"));
-        std::string REQUEST_FAIL(reinterpret_cast<const char*>(u8"获取数据失败"));
+		LOG_TYPE_MAP = config["log_type_map"];
+		LOG_ACTION_MAP = config["log_action_map"];
+		AF_ACTION_MAP = config["af_action_map"];
+		AF_RESULT_MAP = config["af_result_map"];
 
         // 登录
         cpr::Session session;
@@ -93,7 +188,7 @@ int main() {
             std::cout << LOGIN_SUCCESS << std::endl;
 
             // API基础参数
-            // ?action=query&format=json&list=recentchanges|abuselog&formatversion=2&rcprop=title|timestamp|ids|comment|user|loginfo|sizes&rcshow=!bot&rclimit=100&rctype=edit|new|log|external&afllimit=100&aflprop=ids|user|title|action|result|timestamp|revid|filter
+            // ?action=query&format=json&list=recentchanges|abuselog&formatversion=2&rcprop=title|timestamp|ids|comment|user|loginfo|sizes&rcshow=!bot&rclimit=100&rctype=edit|new|log&afllimit=100&aflprop=ids|user|title|action|result|timestamp|revid|filter
             cpr::Payload initial_params = {
                 {"action", "query"},
                 {"format", "json"},
@@ -102,7 +197,7 @@ int main() {
                 {"rcprop", "timestamp|ids"},
                 {"rcshow", "!bot"},
                 {"rclimit", "1"},
-                {"rctype", "edit|new|log|external"},
+                {"rctype", "edit|new|log"},
                 {"afllimit", "1"},
                 {"aflprop", "ids|timestamp"}
             };
@@ -169,37 +264,61 @@ int main() {
                     }
                 }
 
-                // 打印新内容
-                // API沙盒：https://zh.minecraft.wiki/w/Special:API%E6%B2%99%E7%9B%92#action=query&format=json&list=recentchanges%7Cabuselog&formatversion=2&rcprop=title%7Ctimestamp%7Cids%7Ccomment%7Cuser%7Cloginfo%7Csizes&rcshow=!bot&rclimit=1&rctype=log%7Cedit%7Cnew&afllimit=1&aflprop=ids%7Cuser%7Ctitle%7Caction%7Cresult%7Ctimestamp%7Crevid%7Cfilter
-                if (!new_rc_items.empty()) {
-					std::reverse(new_rc_items.begin(), new_rc_items.end());
-                    std::cout << "New Recent Changes:" << std::endl;
-                    for (const auto& item : new_rc_items) {
-                        if (item["type"].get<std::string>() == "log") {
-                            std::cout << LOG_ACTION_MAP[item["logtype"].get<std::string>()] << ": " << std::endl;
-                        } else {
-                            std::cout << "Edit: ";
-						}
-                        std::cout << "Title: " << item["title"].get<std::string>()
-                                  << ", Timestamp: " << item["timestamp"].get<std::string>()
-                                  << ", User: " << item["user"].get<std::string>()
-                                  << ", Comment: " << item["comment"].get<std::string>() << std::endl;
-                    }
+				int is_new_rc = !new_rc_items.empty();
+				int is_new_afl = !new_afl_items.empty();
+                if (is_new_rc) {
+                    std::reverse(new_rc_items.begin(), new_rc_items.end());
                     last_rc_timestamp = new_rc_items.back()["timestamp"].get<std::string>();
                     last_rcid = new_rc_items.back()["rcid"].get<int>();
-				}
-                if (!new_afl_items.empty()) {
+                }
+                if (is_new_afl) {
                     std::reverse(new_afl_items.begin(), new_afl_items.end());
-                    std::cout << "New Abuse Log:" << std::endl;
-                    for (const auto& item : new_afl_items) {
-                        std::cout << "Title: " << item["title"].get<std::string>()
-                                  << ", Timestamp: " << item["timestamp"].get<std::string>()
-                                  << ", User: " << item["user"].get<std::string>()
-                                  << ", Action: " << item["action"].get<std::string>()
-                                  << ", Result: " << item["result"].get<std::string>() << std::endl;
-                    }
                     last_afl_timestamp = new_afl_items.back()["timestamp"].get<std::string>();
-					last_afl_id = new_afl_items.back()["id"].get<int>();
+                    last_afl_id = new_afl_items.back()["id"].get<int>();
+                }
+
+                // 打印新内容
+                // API沙盒：https://zh.minecraft.wiki/w/Special:API%E6%B2%99%E7%9B%92#action=query&format=json&list=recentchanges%7Cabuselog&formatversion=2&rcprop=title%7Ctimestamp%7Cids%7Ccomment%7Cuser%7Cloginfo%7Csizes&rcshow=!bot&rclimit=1&rctype=log%7Cedit%7Cnew&afllimit=1&aflprop=ids%7Cuser%7Ctitle%7Caction%7Cresult%7Ctimestamp%7Crevid%7Cfilter
+                if (is_new_rc && !is_new_afl)// 仅最近更改
+                {
+                    for (const auto& item : new_rc_items) {
+						printrc(item);
+                    }
+                }
+                else if (is_new_afl && !is_new_rc) // 仅滥用日志
+                {
+                    for (const auto& item : new_afl_items) {
+						printafl(item);
+                    }
+                }
+				else if (is_new_rc && is_new_afl) // 同时存在最近更改和滥用日志
+                {
+					/*int is_related = 0;
+                    for (const auto& item : new_afl_items) {
+                        if (item["revid"].get<int>()) {
+							is_related = 1;
+                        }
+                    }*/
+                    // 合并is_new_rc和is_new_afl
+                    std::vector<json> merged_items;
+                    merged_items.reserve(new_rc_items.size() + new_afl_items.size());
+                    merged_items.insert(merged_items.end(), new_rc_items.begin(), new_rc_items.end());
+                    merged_items.insert(merged_items.end(), new_afl_items.begin(), new_afl_items.end());
+
+                    // 按时间顺序排序
+                    std::sort(merged_items.begin(), merged_items.end(), [](const json& a, const json& b) {
+                        return a["timestamp"].get<std::string>() < b["timestamp"].get<std::string>();
+                    });
+
+                    // 输出
+                    for (const auto& item : merged_items) {
+                        if (item.contains("type")) {
+                            printrc(item);
+                        }
+                        else {
+                            printafl(item);
+                        }
+                    }
                 }
             }
         }
